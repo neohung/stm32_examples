f99b22150173c64ba773faf236f9d004f9bbda59
Neo <neo.hung@quantatw.com>
2017-07-24 17:27:02 +0800

    add Motor control command for test_pc

--- test_pc/Src/main.cpp
+++ test_pc/Src/main.cpp
@@ -13,7 +13,11 @@
 
 #include <pthread.h>
 
+#include <ringbuf.h>
 unsigned char Buffer[128];
+ringbuf_t message_in_ringbuf;
+ringbuf_t message_out_ringbuf;
+
 
 usb_dev_handle *open_dev(unsigned short MY_VID, unsigned short MY_PID)
 {
@@ -82,22 +86,23 @@ void usbhid_send(struct usb_dev_handle* usbhandle, int ep, char* buf, unsigned i
 }
 
 
-void usbhid_read(struct usb_dev_handle* usbhandle, int ep, char* buf, unsigned int len)
+bool usbhid_read(struct usb_dev_handle* usbhandle, int ep, char* buf, unsigned int len)
 {
 	const int data_len=255;
+	const int timeout = 1000;
 	char data[data_len];
 	int res;
-	res = usb_interrupt_read(usbhandle, ep, buf, len, -1);
-	//printf( "\nusb_interrupt_read size=%d \n [ ",res );
-	//for(int i=0;i<res;i++) {
-	//	printf("0x%X ", data[i]);
-	//}
-	//printf("]\n");
+	res = usb_interrupt_read(usbhandle, ep, buf, len, timeout);
+	if (res > 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 
-pthread_t Tid1, Tid2;
+pthread_t msg_read_handle, process_msg_in_handle, msg_write_handle, Tid2;
 
-void *thread1(void *arg)
+void *message_read(void *arg)
 {
 	 int s;
 	 s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
@@ -110,16 +115,91 @@ void *thread1(void *arg)
 	char buf[255];
     while (1)
     {
-        //printf("T1T1T1T1T1T1T1T1T1T1T1\n");
-        //fflush(stdout);
     	//set cancel interrupt point
     	pthread_testcancel();
-        usbhid_read(usbhandle, MY_EP_In, buf, 1);
-        printf("buf[0]=[%c]\n",buf[0]);
+        int len = usbhid_read(usbhandle, MY_EP_In, buf, 1);
+        if (len){
+        	unsigned buflen = pushElement(&message_in_ringbuf,buf[0]);
+        	if (len==0){
+        		printf("Full, can't push to message_in_ringbuf any more\n");
+        	}
+        }
         Sleep(1);
     }
     //pthread_exit((void *)1234);
     return NULL;
+}
+
+void *process_msg_in(void *arg)
+{
+	 int s;
+	 s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	 if (s != 0){
+		 printf("pthread_setcancelstate error\n");
+		 exit(EXIT_FAILURE);
+	 }
+	 ringbuf_t* pmsg_in = (ringbuf_t*)arg;
+    while (1)
+    {
+    	//set cancel interrupt point
+    	pthread_testcancel();
+    	unsigned char* pdata;
+    	unsigned int tail = getNextData(pmsg_in, &pdata);
+    	if (pdata){
+    		printf("data[%d]=0x%X\n",(tail-1)&(RINGBUF_MAX_LENGTH-1),*pdata);
+    		//
+    		// Process message here
+    		//
+    	}
+    	Sleep(1);
+    }
+    //pthread_exit((void *)1234);
+    return NULL;
+}
+
+void *message_write(void *arg)
+{
+	int s;
+	s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	if (s != 0){
+		printf("pthread_setcancelstate error\n");
+		exit(EXIT_FAILURE);
+	}
+	struct usb_dev_handle* usbhandle = (struct usb_dev_handle*)arg;
+	int MY_EP_Out = 0x01;
+	while (1)
+	{
+		pthread_testcancel();
+		unsigned char* pdata;
+		unsigned int tail = getNextData(&message_out_ringbuf, &pdata);
+		if (pdata){
+	    		//printf("write data[%d]=[%c]\n",(tail-1)&(RINGBUF_MAX_LENGTH-1),*pdata);
+	    		usbhid_send(usbhandle, MY_EP_Out, (char*)pdata, 1);
+		}
+		Sleep(1);
+	}
+}
+
+void send_message(char* buf,unsigned int len){
+	unsigned int i;
+	char crc = 0xFF;
+	// Pushh Magic Number
+	while (!pushElement(&message_out_ringbuf,0xEA)) {
+		Sleep(1);
+	}
+	crc -= 0xEA;
+	//
+	for(i=0;i<len;i++){
+		while (!pushElement(&message_out_ringbuf,buf[i])) {
+			Sleep(1);
+		}
+		crc -= buf[i];
+	}
+	//push crc
+	while (!pushElement(&message_out_ringbuf,crc)) {
+		Sleep(1);
+	}
+	//
 }
 
 void *thread2(void *arg)
@@ -131,24 +211,56 @@ void *thread2(void *arg)
 		do {
 		    c=getch();
 		    //putchar(c);
-		    usbhid_send(usbhandle, MY_EP_Out, &c, 1);
+		    //usbhid_send(usbhandle, MY_EP_Out, &c, 1);
+		    switch (c)
+		    {
+		    case 'a':
+		    {
+		    	char str[6];
+		    	int linear_speed = 100;
+		    	int angular_speed = 180;
+		    	str[0] = 0x01;  // command type id
+		    	str[1] = 0x4;  // len
+		    	str[2] = linear_speed & 0xFF;
+		    	str[3] = (linear_speed >> 8) & 0xFF;;
+		    	str[4] = angular_speed & 0xFF;
+		    	str[5] = (angular_speed >> 8)& 0xFF;
+		    	send_message(str, sizeof(str));
+		    }
+		    	break;
+		    default:
+		    	send_message(&c,1);
+		    	break;
+		    }
 		    Sleep(1);
 		} while (c != '.');
-		pthread_cancel(Tid1);
+		printf("Do Cancel\n");
+		pthread_cancel(msg_read_handle);
+		pthread_cancel(process_msg_in_handle);
+		pthread_cancel(msg_write_handle);
 		pthread_exit(NULL);
 }
 
 int main(void) {
 	struct usb_dev_handle* usbhandle  = usbhid_start(0x16C0,0x05DF,0);
 	//
+	 ringbufInit(&message_in_ringbuf);
+	 ringbufInit(&message_out_ringbuf);
+	//
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	pthread_create(&Tid1, &attr, thread1,  (void*)usbhandle);
+	pthread_create(&msg_read_handle, &attr, message_read,  (void*)usbhandle);
+	pthread_create(&process_msg_in_handle, &attr, process_msg_in,  (void*)&message_in_ringbuf);
+	pthread_create(&msg_write_handle, &attr, message_write,  (void*)usbhandle);
+
 	pthread_create(&Tid2, &attr, thread2,  (void*)usbhandle);
 	void *ret;
-	pthread_join(Tid1, &ret);
+	pthread_join(msg_read_handle, &ret);
+	pthread_join(process_msg_in_handle, &ret);
+	pthread_join(msg_write_handle, &ret);
 	pthread_join(Tid2, &ret);
+	printf("Tid2 finish\n");
 	pthread_attr_destroy(&attr);
 	//pthread_kill(Tid1, 9); //SIGKILL
 	puts("Finish");
@@ -156,84 +268,3 @@ int main(void) {
 	system("pause");
 	return EXIT_SUCCESS;
 }
-
-/*
-int main(void) {
-	puts("test");
-	usb_init(); // initialize the library
-	usb_find_busses(); // find all busses
-	usb_find_devices(); // find all connected devices
-	struct usb_dev_handle* usbhandle;
-	if (!(usbhandle = open_dev(0x16C0, 0x05DF)))
-	{
-	        printf("error opening device: \n%s\n", usb_strerror());
-	        return 0;
-	}
-	else
-	{
-	    printf("success: device %04X:%04X opened\n", 0x16C0, 0x05DF);
-	}
-    //
-	int MY_INTF = 0;
-	if (usb_claim_interface(usbhandle, MY_INTF) < 0)  //dfu_bInterfaceNumber
-	{
-		printf("error claiming interface #%d:\n%s\n", MY_INTF, usb_strerror());
-		usb_close(usbhandle);
-		exit(0);
-	}
-
-	int res;
-	int MY_EP_In = 0x81;
-	int MY_EP_Out = 0x01;
-	//
-	const int data_len=255;
-	char data[data_len];
-	res = usb_interrupt_read(usbhandle, MY_EP_In, data, data_len, -1);
-	printf( "\nusb_interrupt_read size=%d \n [ ",res );
-	for(int i=0;i<res;i++) {
-		printf("0x%X ", data[i]);
-	}
-	printf("]\n");
-
-	const static int timeout=5000; // timeout in ms
-	const static int str_len = 2;
-	char str[str_len];
-	str[0] = 0x11;
-	str[1] = 0x22;
-
-	res = usb_interrupt_write(usbhandle, MY_EP_Out, str, str_len, timeout);
-	if( res < 0 )
-	{
-	     perror("USB interrupt write");
-	}
-
-	//printf("Dats=0x%X\n",data[0]);
-*/
-	/*
-	#define USB_ENDPOINT_IN         MY_EP
-    #define USB_TYPE_CLASS          (0x01 << 5)
-    #define USB_RECIP_INTERFACE        0x01
-    #define HID_REPORT_GET          0x01
-	char recv_data[5];
-	#define USB_TIMEOUT     10000
-	int recv_len = usb_control_msg(usbhandle,
-	USB_ENDPOINT_IN + USB_TYPE_CLASS + USB_RECIP_INTERFACE,
-	                               HID_REPORT_GET,
-	                               0x300,
-	                                 0,
-	                               recv_data,5, USB_TIMEOUT);
-	printf( "\nusb_interrupt_read %d \n",recv_len );
-		for(int i=0;i<recv_len;i++) {
-			printf("0x%X ", recv_data[i]);
-		}
-		printf("\n");
-		printf("Report_ID=%d, X=%d,Y=%d\n",recv_data[0],recv_data[2],recv_data[3]);
-   */
-/*
-	puts("Finish");
-	usb_release_interface(usbhandle, MY_INTF);
-	usb_close(usbhandle);
-	system("pause");
-	return EXIT_SUCCESS;
-}
-*/
