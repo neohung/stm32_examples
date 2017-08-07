c4e8b62512268ead81ff0b4cc34f6d08b40f19aa
Neo <neo.hung@quantatw.com>
2017-08-07 18:07:24 +0800

    test

--- test_pc_by_socket/Src/main.cpp
+++ test_pc_by_socket/Src/main.cpp
@@ -101,6 +101,33 @@ bool usbhid_read(struct usb_dev_handle* usbhandle, int ep, char* buf, unsigned i
 
 pthread_t msg_read_handle, process_msg_in_handle, msg_write_handle, Tid2, Tid1;
 
+void *message_write(void *arg)
+{
+	//Sleep(10);
+	int s;
+	s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	if (s != 0){
+		printf("pthread_setcancelstate error\n");
+		exit(EXIT_FAILURE);
+	}
+	SOCKET* psConnect = (SOCKET*)arg;
+	while (1)
+	{
+		//printf("message_write\n");
+		pthread_testcancel();
+		unsigned char* pdata;
+		//unsigned int tail =
+				getNextData(&message_out_ringbuf, &pdata);
+		if (pdata){
+	    		//printf("write data[%d]=[%c]\n",(tail-1)&(RINGBUF_MAX_LENGTH-1),*pdata);
+	    		//usbhid_send(usbhandle, MY_EP_Out, (char*)pdata, 1);
+			send(*psConnect, (char*)pdata, 1, 0);
+		}
+		Sleep(1);
+	}
+	//	closesocket(sConnect);
+}
+
 void *message_read(void *arg)
 {
 	 int s;
@@ -109,15 +136,19 @@ void *message_read(void *arg)
 		 printf("pthread_setcancelstate error\n");
 		 exit(EXIT_FAILURE);
 	 }
-	struct usb_dev_handle* usbhandle = (struct usb_dev_handle*)arg;
-	int MY_EP_In = 0x81;
+	 SOCKET* psConnect = (SOCKET*)arg;
+	//struct usb_dev_handle* usbhandle = (struct usb_dev_handle*)arg;
+	//int MY_EP_In = 0x81;
 	char buf[255];
     while (1)
     {
+    	//printf("message_read\n");
     	//set cancel interrupt point
     	pthread_testcancel();
-        int len = usbhid_read(usbhandle, MY_EP_In, buf, 1);
-        if (len){
+        //int len = usbhid_read(usbhandle, MY_EP_In, buf, 1);
+        int len = recv(*psConnect, (char*)buf, sizeof(buf), 0);
+        if (len > 0){
+        	printf("message_read: len=%d\n",len);
         	unsigned buflen = pushElement(&message_in_ringbuf,buf[0]);
         	if (buflen==0){
         		printf("Full, can't push to message_in_ringbuf any more\n");
@@ -141,7 +172,7 @@ unsigned char polling_data(void)
 	    		//printf("data[%d]=0x%X\n",(tail-1)&(RINGBUF_MAX_LENGTH-1),*pdata);
 	    		break;
 	    }else{
-	    	Sleep(1);
+	    	Sleep(10);
 	    }
 	}
 	return *pdata;
@@ -202,14 +233,14 @@ OiCmdDispatcher oiOpChecker[] =
 void processCommand(unsigned char Command_type_id, char* arg ,unsigned char arg_len)
 {
 	unsigned int i;
-	/*
+/*
 	printf("Command_type_id=%d\n",Command_type_id);
 	printf("arg_len=%d\n",arg_len);
 	for(i=0;i<arg_len;i++){
 		printf("0x%X ", arg[i]);
 	}
 	printf("\n");
-	*/
+*/
 	//
 	for(i=0;i<NUMBER_SUPPORTED_OI_CMD;i++)
 	{
@@ -236,11 +267,13 @@ void *process_msg_in(void *arg)
     	pthread_testcancel();
     	char checksum = 0xFF;
     	do{
+    		Sleep(1);
     	}while(polling_data() != 0xEA);
+    	printf("polling_data ==0xEA\n");
     	checksum -= 0xEA;
     	unsigned char Command_type_id = polling_data();
     	checksum -= Command_type_id;
-    	//printf("Command_type_id: %d\r\n",Command_type_id);
+    	printf("Command_type_id: %d\r\n",Command_type_id);
     	unsigned char len = polling_data();
     	checksum -= len;
     	// Check len if make sense
@@ -260,35 +293,12 @@ void *process_msg_in(void *arg)
     		printf("read crc=0x%X, should be 0x%X\r\n",crc,checksum);
     		continue;
     	}
+    	printf("Call processCommand\n");
     	// Pass command_type id and param[] and len
     	processCommand(Command_type_id, param, len);
     }
     //pthread_exit((void *)1234);
     return NULL;
-}
-
-void *message_write(void *arg)
-{
-	int s;
-	s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-	if (s != 0){
-		printf("pthread_setcancelstate error\n");
-		exit(EXIT_FAILURE);
-	}
-	struct usb_dev_handle* usbhandle = (struct usb_dev_handle*)arg;
-	int MY_EP_Out = 0x01;
-	while (1)
-	{
-		pthread_testcancel();
-		unsigned char* pdata;
-		//unsigned int tail =
-				getNextData(&message_out_ringbuf, &pdata);
-		if (pdata){
-	    		//printf("write data[%d]=[%c]\n",(tail-1)&(RINGBUF_MAX_LENGTH-1),*pdata);
-	    		usbhid_send(usbhandle, MY_EP_Out, (char*)pdata, 1);
-		}
-		Sleep(1);
-	}
 }
 
 //MUTEX
@@ -526,27 +536,140 @@ void *thread2(void *arg)
 //#include <sys/socket.h>
 //#include <unistd.h>
 #endif
-
-void *thread1(void *arg)
+pthread_t server_tid;
+void *socketserver_thread(void *arg)
 {
-	char c;
+	printf("socketserver_thread\n");
+	 int* pPORT = (int*)arg;
+	 int PORT = *pPORT;
 #ifdef WIN32
-	WSADATA wsadata;
-	if (WSAStartup(MAKEWORD(1,1), &wsadata) == SOCKET_ERROR) {
-		printf("Error creating socket.");
-		return NULL;
+	//----------------- Init WS2 DLL -----------------
+	WSAData wsaData;
+	WORD version = MAKEWORD(2, 2);
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0) {
+	  printf("Init WS2 fail\n");
+	  return NULL;
 	}
 #endif
-
-	puts ("A dot ('.') to exit:");
-	do {
-		c=getch();
-		Sleep(50);
-	} while (c != '.');
-	printf("Do Finish\n");
-	pthread_exit(NULL);
-	return NULL;
+	//-----------------  Create Socket ----------------
+	SOCKET sListen = INVALID_SOCKET;
+	sListen= socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sListen== INVALID_SOCKET) {
+	  printf("Create Socket fail\n");
+	  return NULL;
+	}
+	//-----------------  Set SOCKADDR_IN and PORT ----------------
+	SOCKADDR_IN addr;
+	memset (&addr, 0, sizeof (addr)) ;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	// addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	//-----------------  BIND ----------------
+	int r = bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	if (r == SOCKET_ERROR){
+	  printf("Bind Port %d fail\n",PORT);
+	  return NULL;
+	}
+	//-----------------  LISTEN ----------------
+	r = listen(sListen, SOMAXCONN);
+	if (r == SOCKET_ERROR){
+	  printf("Listen Port %d fail\n",PORT);
+	  return NULL;
+	}
+	SOCKET sConnect;
+	struct sockaddr_in clientAddr;
+	int clientAddrLen = sizeof(clientAddr);
+	printf("Server Ready to got accetp\n");
+	sConnect = accept(sListen, (SOCKADDR*)&clientAddr, &clientAddrLen);
+	printf("Server got accetp\n");
+		if (sConnect != INVALID_SOCKET)
+		{
+		  printf("server: got connection from %s\n", inet_ntoa(clientAddr.sin_addr));
+		}
+	//
+	while(1){
+		pthread_testcancel();
+		unsigned char message[200];
+		ZeroMemory(message, 200);
+		int n = recv(sConnect, (char*)message, sizeof(message), 0);
+		if (n > 0){
+		  int i;
+		  printf("Size %d\n[ ",n);
+		  for (i=0;i<n;i++)
+		    printf("0x%X ",message[i]);
+		  printf(" ]\n");
+		}
+		//(message[0] == 0xEA)
+		if ((message[0] == 0x03) && (message[1] == 0x0)&& (message[2] == 0x12))
+		{
+		  printf("It is Query command\n");
+		  int x = 0x0001; //mm
+		  	int y = 0x0002; //mm
+		  	int theta = 0x0003; //degree 0~360
+		  	int linear_speed = 0x0004; //mm
+		  	int angular_speed = 0x0005; //mm
+		  message[0] = 0xEA;
+		  message[1] = OI_OPCODE_ODOMUPDATE;
+		  message[2] = 10; //len
+		  message[3] = x & 0xFF;
+		  message[4] = (x >> 8) & 0xFF;
+		  message[5] = y & 0xFF;
+		  message[6] = (y >> 8)& 0xFF;
+		  message[7] = theta & 0xFF;
+		  message[8] = (theta >> 8) & 0xFF;
+		  message[9] = linear_speed & 0xFF;
+		  message[10] = (linear_speed >> 8) & 0xFF;
+		  message[11] = angular_speed & 0xFF;
+		  message[12] = (angular_speed >> 8)& 0xFF;
+		  int i=0;
+		  char crc=0xFF;
+		  for (i=0;i<13;i++){
+			  crc -=  message[i];
+		  }
+		  message[13] = crc;
+		  send(sConnect, (char*)message, 14,0);
+		}
+		Sleep(10);
+	}
 }
+
+SOCKET to_connection(void)
+{
+	printf("to_connection\n");
+		Sleep(10);
+		//Do connection
+		SOCKADDR_IN addr;
+		SOCKET sConnect;
+		sConnect = socket(AF_INET, SOCK_STREAM, 0);
+		//set the socket I/O mode
+		//if iMode = 0, blocking is enabled
+		//if iMode != 0, non-blocking mode is enabled
+		u_long iMode = 1;
+		ioctlsocket(sConnect, FIONBIO, &iMode);
+		addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(1111);
+		int ret;
+		do {
+		  ret = connect(sConnect, (SOCKADDR*)&addr, sizeof(addr));
+		  if (ret){
+			if( WSAGetLastError() == WSAEWOULDBLOCK){
+				printf("Attempting to connect. %d\n",ret);
+				ret = 0;
+			}else{
+				 Sleep(1000);
+				//printf("Failed to connect to server.\n");
+				//printf("Error: %d\n",WSAGetLastError());
+				//WSACleanup();
+			}
+		  }
+		}while(ret == SOCKET_ERROR);
+		printf("connected, ret = %d\n",ret);
+		return sConnect;
+}
+
 int main(void) {
 	//struct usb_dev_handle* usbhandle  = usbhid_start(0x16C0,0x05DF,0);
 	//
@@ -565,7 +688,19 @@ int main(void) {
 	//pthread_create(&process_msg_in_handle, &attr, process_msg_in,  (void*)&message_in_ringbuf);
 	//-------------------------------------------------------------------------
 	//pthread_create(&Tid2, &attr, thread2,  (void*)usbhandle);
-	pthread_create(&Tid2, &attr, thread1,  NULL);
+	//--------------------
+	//Server Emu
+	int PORT=1111;
+	pthread_create(&server_tid, &attr, socketserver_thread, (void*)&PORT);
+	//--------------------
+	pthread_create(&Tid2, &attr, thread2,  NULL);
+	//
+	//Process Connection
+	SOCKET s = to_connection();
+	//
+	pthread_create(&msg_write_handle, &attr, message_write,  &s);
+	pthread_create(&msg_read_handle, &attr, message_read,  &s);
+	pthread_create(&process_msg_in_handle, &attr, process_msg_in,  (void*)&message_in_ringbuf);
 	//-------------------------------------------------------------------------
 	//pthread_create(&Tid1, &attr, send_query_thread,  (void*)usbhandle);
 	//------------------------------------------------------------------------
